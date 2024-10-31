8c580ffbd9a74827d37997981ea6ea1687cd2fc1
Shreekant Chavan <shreekantchavanfycs@gmail.com>
2024-10-31 16:09:26 +0530

    Final Touch

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App() {
 
   return (
     <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggelCompelete}}>
-      <div className="bg-[#172842] min-h-screen py-8">
+      <div className="bg-[#172842] min-h-screen w-screen py-8 flex justify-center overflow-y-hidden">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
@@ -54,12 +54,6 @@ function App() {
                     </div>
                 </div>
             </div>
-        <div className="flex flex-wrap min-h-screen items-center">
-          <div className="w-full">
-            <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
-              <div className="w-full max-w-sm mx-auto"></div>
-          </div>
-        </div>
     </TodoProvider>
   )
 }
